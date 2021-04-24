90d0b18349437d832ef471004f875ab0bc87930e
Mr.BAD <79611127+mr-bad1700@users.noreply.github.com>
2021-04-24 23:07:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -583,7 +583,7 @@ const ghallat = "";
 const ghallatw = "";
 const logosec = "";
 const warn = "";
-const color = "#58555A";
+const color = "#404040";
 
 let anti = JSON.parse(fs.readFileSync("./antigreff.json", "UTF8"));
 let config = JSON.parse(fs.readFileSync("./configg.json", "UTF8"));
