5a3dfe57da63c7d21512d9fac74928a1c8b57333
Mr.BAD <79611127+mr-bad1700@users.noreply.github.com>
2021-04-30 21:38:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -41,17 +41,11 @@ const cooldown = new Set();
 const cdtime = 7;
 client.login("NzY0NDg3NDE2NzQ4MzEwNTcw.X4G-Yw.0spctXCwJ5FOUsXCV7YLB638vd4");
 
-
-// 
-client.on('ready', () => {
-    console.log('MrFox is ready')
-    client.user.setPresence({
-        activity: {
-            type: "PLAYING",
-            name: `${prefix}help - https://mrfox.ga/`
-        }, 
-        status: "idle"
-    })
+client.on("ready", () => {
+  client.user.setActivity(`${prefix}help - https://mrfox.ga/`, {
+    type: "LISTENING"
+  }); ////////////////
+  client.user.setStatus("ONLINE");
 });
 
 
