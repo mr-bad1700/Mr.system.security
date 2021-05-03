f66b15009d6ecce03679a1da94df671a8e1132db
Mr.BAD <79611127+mr-bad1700@users.noreply.github.com>
2021-05-03 17:48:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -54,106 +54,29 @@ setInterval(() => {
 //An join announcement for everyone but no one knows so fine ^w^
 
 client.on("guildCreate" , bot => {
-  if(bot.memberCount < 110 ){
+  if(bot.memberCount < 1 ){
     bot.leave();
   }
 })
 
 
 
-
-
-
-  
-
-// anime
-const cheerio = require('cheerio');
-
-
-
-client.on("message", async message => {
-  if (message.content === "m/anime") {
-    let random = Math.floor(Math.random() * 107)
-    let text = await fetch(`https://hdqwalls.com/category/anime-wallpapers/page/${random}`, { method: "GET" })
-    text = await text.text()
-    let $ = cheerio.load(text)
-    let images = []
-
-    $('img[class="thumbnail img-responsive custom_width"]').each(function(i, elem) {
-      images.push($(this).attr('src'));
-    });
-
-
-
-    let limit = images.length
-    let pg = 0;
-    let embed = new discord.MessageEmbed()
-      .setColor("#000001")
-      .setImage(images[0].replace("/thumb", ""))
-
-    const msg = await message.channel.send(embed);
-
-    msg.react("⬅️");
-    await msg.react("➡️");
-
-    const collector = msg.createReactionCollector(
-      // only collect left and right arrow reactions from the message author
-      (reaction, user) =>
-        ["⬅️", "➡️"].includes(reaction.emoji.name) &&
-        user.id === message.author.id,
-      // time out after a minute
-      { time: 120000 }
-    );
-
-    collector.on("collect", reaction => {
-
-      reaction.users.remove(message.author).then(async () => {
-        if (reaction.emoji.name === "➡️") {
-
-
-          if (!images[pg + 1]) {
-            pg = pg
-          } else {
-            pg = pg + 1
-          }
-          embed = new discord.MessageEmbed()
-            .setColor("#000001")
-            .setImage(images[pg].replace("/thumb", ""))
-          msg.edit(embed)
-          msg.react("⬅️");
-          await msg.react("➡️");
-        }
-
-        else if (reaction.emoji.name === "⬅️") {
-
-
-          if (!images[pg - 1]) {
-
-            pg = pg
-          } else {
-            pg = pg - 1
-          }
-
-          embed = new discord.MessageEmbed()
-            .setColor("#000001")
-            .setImage(images[pg].replace("/thumb", ""))
-          msg.edit(embed)
-
-          msg.react("⬅️");
-          await msg.react("➡️");
-        }
-      })
-
-    })
-
-      collector.on('end', collected => {
-      if (msg) {
-        msg.reactions.removeAll()
+client.on('message', message => {
+    if (message.content.startsWith(prefix + "avatar")) {
+if(!message.channel.guild) return;
+        var mentionned = message.mentions.users.first();
+    var client;
+      if(mentionned){
+          var client = mentionned; } else {
+          var client = message.author;
       }
-    });
-
-  }
-})
+        const embed = new Discord.MessageEmbed()
+                .addField('Requested by:', "<@" + message.author.id + ">")
+        .setColor(color)
+        .setImage(`${client.avatarURL()}`)
+      message.channel.send(embed);
+    }
+});
 
 
   
