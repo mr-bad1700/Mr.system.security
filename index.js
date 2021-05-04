15b40fdbae795e6294de062855ff2c35bc8cd44b
Mr.BAD <79611127+mr-bad1700@users.noreply.github.com>
2021-05-04 05:06:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -290,6 +290,35 @@ let result = Math.floor((Math.random() * replies.length));
 });
 
 
+client.on('message', message => {
+  const codework = message.mentions.users.first()  
+  let user;
+  
+   let replies = ["https://cdn.discordapp.com/attachments/810651927334748180/811017898860347422/image0.gif", "https://cdn.discordapp.com/attachments/810651927334748180/811017900076040193/GIF-201127_023626.gif", "https://cdn.discordapp.com/attachments/742107775475253259/819268083667566622/image0.gif", "https://cdn.discordapp.com/attachments/810651927334748180/810652801218117672/siyah.gif", "https://cdn.discordapp.com/attachments/810651927334748180/810652090979844096/image0.gify", "https://cdn.discordapp.com/attachments/810651927334748180/810652685061062663/68.gif", "https://cdn.discordapp.com/attachments/810651927334748180/810652708926390292/uzgun-19.gif", "https://cdn.discordapp.com/attachments/810651927334748180/811012110145290310/a_1ba840ca0fdc749b381714af5f3939d9.gif", "https://cdn.discordapp.com/attachments/810651927334748180/810652664278810634/eniippbence.gif", "https://cdn.discordapp.com/attachments/810651927334748180/811012110653063238/GIF-201114_165700.gif", "https://cdn.discordapp.com/attachments/742107775475253259/819268121974014052/image0.gif", "https://cdn.discordapp.com/attachments/752914781941268563/825004389189025802/image0.gif", "https://cdn.discordapp.com/attachments/742107775475253259/821090832735928350/image0-24.gif", "https://cdn.discordapp.com/attachments/810651927334748180/811012066491629578/image0-34.gif", "https://cdn.discordapp.com/attachments/805854616511316014/821028902801506334/image0.gif", "https://cdn.discordapp.com/attachments/810651927334748180/810651982611742750/image0.gif"];
+
+let result = Math.floor((Math.random() * replies.length));
+
+  if (message.mentions.users.first())  {user = message.mentions.users.first();}
+  else {user = message.author;}
+ var args = message.content.split(" ").slice(1);
+   if(message.content.startsWith(prefix + 'sad')) {
+    if(!message.channel.guild) return message.reply('**This command only for servers **');
+    var gif = new Discord.MessageEmbed()
+         
+         .setTitle("> __Sad Gif__")
+         .setURL(`https://mrfox.ga/`)
+         .setDescription(`> tag: <@${user.id}>`)
+         .setColor(color)
+         .setFooter(`Request by: ${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
+         .setAuthor(client.user.username, client.user.avatarURL())
+         .setImage(replies[result])
+          
+      message.channel.send(gif);
+ 
+    }
+});
+
+
 
 
 
