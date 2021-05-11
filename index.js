c9b2d51d7793574fa1fa3a5dad11628bb5e0a4bc
Mr.BAD <79611127+mr-bad1700@users.noreply.github.com>
2021-05-11 17:42:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -613,38 +613,11 @@ client.on("message", message => {
     if(!message.channel.guild) return message.reply('```This command only for servers```');
     let Dashboard = `
 __Music Commands__ 
-
-leave - Leave The Voice Channel!
-
-loop - Toggle music loop
-
-lyrics - Get lyrics for the currently playing song
-
-nowplaying - To show the music which is currently playing in this server
-
-pause - To pause the current music in the server
-
-play - To play songs
-
-playlist - To play songs
-
-queue - To show the server songs queue
-
-remove - Remove song from the queue
-
-resume - To resume the paused music
-
-search - To search songs
-
-shuffle - Shuffle queue
-
-skip - To skip the current music
-
-skipto - Skip to the selected queue number
-
-stop - To stop the music and clearing the queue
-
-volume - To change the server song queue volume
+> loop - lyrics - nowplaying
+> pause - play - playlist
+> queue - remove - resume
+> search - shuffle - skip
+> skipto - stop - volume - leave
 __This is a little prefix__
 > [ ${prefix} ]
 `;
